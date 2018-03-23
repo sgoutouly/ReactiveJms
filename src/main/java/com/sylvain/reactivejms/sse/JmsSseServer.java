3c6fc4fb6d388f5f7b1366eb100c2c270ae95a5a
sgoutouly <sgoutouly@gmail.com>
2018-03-23 18:04:44 +0100

    Adding the rabbit !

--- src/main/java/com/sylvain/reactivejms/sse/JmsSseServer.java
+++ src/main/java/com/sylvain/reactivejms/sse/JmsSseServer.java
@@ -1,12 +1,21 @@
 package com.sylvain.reactivejms.sse;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
-import io.netty.buffer.ByteBuf;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.logging.LogLevel;
 import io.reactivex.netty.protocol.http.server.HttpServer;
 import io.reactivex.netty.protocol.http.server.HttpServerResponse;
 import io.reactivex.netty.protocol.http.sse.ServerSentEvent;
@@ -15,58 +24,91 @@ import rx.subjects.PublishSubject;
 
 /**
  * 
- * 
  * @author sylvain
  */
 public final class JmsSseServer {
 
-    public static void main(final String[] args) {
-
-        Map<String, HttpServerResponse<ServerSentEvent>> clients = new HashMap<>();
+	private static Channel channel; 
+	private static Connection connection;
+	
+	public static void connectToRabbit() throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+		connection = factory.newConnection();
+		channel = connection.createChannel();
+	}
+	
+    public static void main(final String[] args) throws IOException, TimeoutException, UnsupportedEncodingException  {
+    	
     	PublishSubject<String> dataStream = PublishSubject.create();
     	
-    	HttpServer<ByteBuf, ByteBuf> server;
-
-    	server = HttpServer.newServer(9000)
-           .enableWireLogging("sse-server", LogLevel.INFO)
-           .start((req, resp) -> {
-        	   
-        	   // SSE endPoint
-        	   if (req.getUri().startsWith("/connect")) {
-            	   	HttpServerResponse<ServerSentEvent> sseStream = resp.transformToServerSentEvents();
-            	   	
-            	   	clients.put(sseStream.toString(), sseStream);
-            	   	System.out.println("Nb clients connectés : " +  (clients.size() + 1));
-            	    
-            	   	return sseStream.writeAndFlushOnEach(dataStream
-            	   		.onBackpressureBuffer(10)
-                		.map(m -> ServerSentEvent.withData(m + "\n")));
-        	    }
-        	   // Pusht data to SSE endPoint
-        	    else if(req.getUri().startsWith("/push")) {
-        	    	dataStream.onNext("One message ! " + UUID.randomUUID().toString());
-        	    	return resp.writeString(Observable.just("Data sent !"));
-        	    }
-        	   // Index
-        	    else if(req.getUri().startsWith("/")) {
-        	    	StringBuffer html = new StringBuffer()
-        	    		.append("<script>").append("\n")
-    	    			.append("var sse = new EventSource(\"http://localhost:9000/connect\");").append("\n")
-    	    			.append("sse.onmessage = function(event) { console.log(\"SSE message : \" + event.data); };").append("\n")
-    	    			.append("sse.onopen = function() { console.log(\"SSE connected !\"); };").append("\n")
-    	    			.append("</script>");
-        	    	return resp.writeString(Observable.just(html.toString()));
-        	    }
-        	   // Not Found handler
-        	    else {
-        	    	resp.setStatus(HttpResponseStatus.NOT_FOUND);
-        	    	return resp.writeAndFlushOnEach(Observable.empty());
-        	    }
-        	   
-           	}
-          );
-        
-        server.awaitShutdown();
+    	connectToRabbit();
+    	
+    	new Thread(new Runnable() {
+    		@Override
+    		public void run() {
+    			System.out.println("Démarrage du listener ...");
+    			Consumer consumer = new DefaultConsumer(channel) {
+    			      @Override
+    			      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+    			          throws IOException {
+    			        String message = new String(body, "UTF-8");
+    			        System.out.println(" [x] Received '" + message + "'");
+    			        dataStream.onNext("One message ! " + UUID.randomUUID().toString());
+    			      }
+    			    };
+    			try {
+    				channel.basicConsume("hello", true, consumer);
+    			} 
+    			catch (IOException e) {
+    			}
+    			System.out.println("... listener démarré !");			
+    		}
+    	}).start();
+    	
+    	
+    	Map<String, HttpServerResponse<ServerSentEvent>> clients = new HashMap<>();
+    	
+    	HttpServer.newServer(9000).start((req, resp) -> {
+    		// SSE endPoint
+    	   if (req.getUri().startsWith("/connect")) {
+        	   	HttpServerResponse<ServerSentEvent> sseStream = resp.transformToServerSentEvents();
+        	   	
+        	   	clients.put(sseStream.toString(), sseStream);
+        	   	System.out.println("Nb clients connectés : " +  (clients.size() + 1));
+        	    
+        	   	return sseStream.writeAndFlushOnEach(dataStream
+        	   		.onBackpressureBuffer(10)
+            		.map(m -> ServerSentEvent.withData(m + "\n")));
+    	    }
+    	    // Pusht data to SSE endPoint
+    	    else if(req.getUri().startsWith("/push")) {
+    	    	//dataStream.onNext("One message ! " + UUID.randomUUID().toString());
+    	    	try {
+    	    		System.out.println("Envoi d'un message !");
+					channel.basicPublish("", "hello", null, ("One message ! " + UUID.randomUUID().toString()).getBytes());
+				} 
+    	    	catch (IOException e) {}
+    	    	
+    	    	return resp.writeString(Observable.just("Data sent !"));
+    	    }
+    	    // Index
+    	    else if(req.getUri().startsWith("/")) {
+    	    	StringBuffer html = new StringBuffer()
+    	    		.append("<script>").append("\n")
+	    			.append("var sse = new EventSource(\"http://localhost:9000/connect\");").append("\n")
+	    			.append("sse.onmessage = function(event) { console.log(\"SSE message : \" + event.data); };").append("\n")
+	    			.append("sse.onopen = function() { console.log(\"SSE connected !\"); };").append("\n")
+	    			.append("</script>");
+    	    	return resp.writeString(Observable.just(html.toString()));
+    	    }
+    	    // Not Found handler
+    	    else {
+    	    	resp.setStatus(HttpResponseStatus.NOT_FOUND);
+    	    	return resp.writeAndFlushOnEach(Observable.empty());
+    	    }
+    	   
+       	}).awaitShutdown();
         
     }
 }
